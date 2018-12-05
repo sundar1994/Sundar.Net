2fae7220beaeee4d57c0d2fb0356ee3729e17791
Sundar <85296392@qq.com>
2018-12-05 13:23:30 +0800

    api添加aotofac支持

--- Sundar.WebApi/App_Start/WebApiConfig.cs
+++ Sundar.WebApi/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace Sundar.WebApi
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
