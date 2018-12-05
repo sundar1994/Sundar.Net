d9aea0e02b5c7dfc06324c156dd76fd50392856a
Sundar <85296392@qq.com>
2018-12-05 10:35:19 +0800

    webapi添加SSO

---
+++ Sundar.BLL/SSO/AppInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sundar.BLL.SSO
+{
+    /// <summary>
+    /// 应用程序信息
+    /// </summary>
+    public class AppInfo
+    {
+        public string AppKey { get; set; }
+
+        public string AppSecret { get; set; }
+
+        public string Title { get; set; }
+
+        public string Remark { get; set; }
+
+        public string Icon { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public bool IsEnable { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+}
---
+++ Sundar.WebApi/Areas/SSO/SSOAreaRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace Sundar.WebApi.Areas.SSO
+{
+    /// <summary>
+    /// SSO区域注册
+    /// </summary>
+    public class SSOAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SSO";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SSO_default",
+                "SSO/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
